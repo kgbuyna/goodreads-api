bc14dcec1db3552367d242bad9885266d50a474d
kgbuyna <kgbuyna692@gmail.com>
2023-01-12 18:43:36 +0800

    ...

--- controllers/wantToRead.js
+++ controllers/wantToRead.js
@@ -1,56 +1,53 @@
-const asyncHandler = require('../middleware/asyncHandler');
-const {wantToReads} = require('../Models/Book');
+const asyncHandler = require("../middleware/asyncHandler");
+const { wantToReads } = require("../Models/Book");
 
-
-
-exports.getBook = asyncHandler( async (req, res, next)=>{
-    let query;
-    console.log("Get request");
-    query = wantToReads.find(req.id);
-    const books = await query; 
-    if(books){
-        res.status(200).json({
-            success: true, 
-            data: books
-        })
-    }
-})
-
-exports.getBooks = asyncHandler( async (req, res, next)=>{
-    let query;
-    console.log("Get request");
-    query = wantToReads.find();
-    const books = await query; 
-    if(books){
-        res.status(200).json({
-            success: true, 
-            data: books
-        })
-    }
-})
-
-
-exports.addToList = asyncHandler( async(req, res, next) =>{
-    req.body.id = req.body._id;
-    console.log(req.body);
-    // console.log(req.body); 
-    // Ийшээ бол дамжуулж байгаа шдээ. Гэхдээ нөгөө хэсэг нь хаана байна вэ? req р ч гэсэн орж ирж байгаа 
-    // localhost дээр ажиллаж байна уу? шалгаад үзэх
-
-    const book = await wantToReads.create(req.body);
-    console.log("");
+exports.getBook = asyncHandler(async (req, res, next) => {
+  let query;
+  console.log("Get request");
+  query = wantToReads.find(req.id);
+  const books = await query;
+  if (books) {
     res.status(200).json({
-        success:true,
-        data: book,
-    })
-})
+      success: true,
+      data: books,
+    });
+  }
+});
 
-exports.removeFromList = asyncHandler( async(req, res, next) =>{    
-    const book = await wantToReads.deleteOne(req.id);
+exports.getBooks = asyncHandler(async (req, res, next) => {
+  let query;
+  console.log("Get request");
+  query = wantToReads.find();
+  const books = await query;
+  if (books) {
     res.status(200).json({
-        success:true,
-        data: book,
-        message:'Устгагдлаа.'
-    })
-})
+      success: true,
+      data: books,
+    });
+  }
+});
+
+exports.addToList = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
+
+  const obj = req.body;
+
+  obj["id"] = obj["_id"];
+  delete obj["_id"];
   
+  const book = await wantToReads.create(obj);
+  console.log("");
+  res.status(200).json({
+    success: true,
+    data: book,
+  });
+});
+
+exports.removeFromList = asyncHandler(async (req, res, next) => {
+  const book = await wantToReads.deleteOne(req.id);
+  res.status(200).json({
+    success: true,
+    data: book,
+    message: "Устгагдлаа.",
+  });
+});
